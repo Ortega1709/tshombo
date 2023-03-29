fa37e480518fd8d531d9a8316e092d214eb7b2d8
Ortega Kabwe <kabweortega@gmail.com>
2023-03-29 14:05:29 +0200

    first commit

---
+++ app/src/main/java/com/ortega/tshombo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ortega.tshombo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
