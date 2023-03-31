6420413a31f96b742412b0f91cbc407e559ad8c0
Ortega Kabwe <kabweortega@gmail.com>
2023-03-31 16:32:00 +0200

    design app

--- app/src/main/java/com/ortega/tshombo/MainActivity.java
+++ app/src/main/java/com/ortega/tshombo/MainActivity.java
@@ -1,17 +1,45 @@
 package com.ortega.tshombo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ortega.tshombo.models.MarqueModel;
+
+import java.util.Objects;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText nomEditText;
+    MaterialButton sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.initialize();
+
+
     }
+
+    private void initialize() {
+
+
+
+    }
+
 }
---
+++ app/src/main/java/com/ortega/tshombo/models/MarqueModel.java
@@ -0,0 +1,17 @@
+package com.ortega.tshombo.models;
+
+public class MarqueModel {
+
+    private String nom;
+
+    public MarqueModel(String nom) {
+        this.nom = nom;
+    }
+
+    public MarqueModel() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+}
